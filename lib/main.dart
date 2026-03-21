469f5ff4790875ebbafd6f669160557e53ebdeaf
Angel96z <77423458+Angel96z@users.noreply.github.com>
2026-03-21 12:51:35 +0100

    Collega la sezione Gioca alla nuova lobby

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app/app.dart';
 import 'app/di/app_dependencies.dart';
@@ -6,5 +7,5 @@ import 'app/di/app_dependencies.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppDependencies.initialize();
-  runApp(const DartsApp());
+  runApp(const ProviderScope(child: DartsApp()));
 }
